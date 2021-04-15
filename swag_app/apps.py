3a940d2ccefe5d4f15caa596a54f2466926306c6
Rosie <yeahhimrosie@gmail.com>
2021-04-14 19:00:32 -0700

    base project

---
+++ swag_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SwagAppConfig(AppConfig):
+    name = 'swag_app'
---
+++ swag_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
