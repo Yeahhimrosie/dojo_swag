855dbf92b06a9c515fc7a77010ebb30ef97d71ab
AbbeyC <abbey.chrystal@gmail.com>
2021-04-20 22:40:16 -0700

    Assignment notes added

--- swag_app/views.py
+++ swag_app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 
 # Create your views here.
+
+#Marj will handle views
 def index(request):
     return render(request, 'index.html')
