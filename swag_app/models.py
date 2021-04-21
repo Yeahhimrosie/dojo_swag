a0e57bef56b22e6e60e1a7f4669c97242e6cad2d
AbbeyC <abbey.chrystal@gmail.com>
2021-04-21 00:17:44 -0700

    added Product class to models and set up image attribute/uploads paths and folders

--- swag_app/models.py
+++ swag_app/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import user
 
 # Create your models here.
 # Abbey will handle models
+
+class Products(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField(default=0.00)
+    description = models.TextField()
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
