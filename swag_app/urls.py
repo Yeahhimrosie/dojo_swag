54f5017427abb911094c7ff6c4d43d940a8586c0
Royrocks <rlhudson@ucdavis.edu>
2021-04-21 01:07:23 -0700

    Made edits to urls.py

--- swag_app/urls.py
+++ swag_app/urls.py
@@ -5,6 +5,12 @@ from . import views
 #Roy will handle urls
 
 urlpatterns = [
-    path('', views.index),
-    path('/dojoswag', views.swag_home)
+    path('dojoswag', views.swag_home),
+    path('dojoswag/checkoutpage', views.checkout_page),
+    path('dojoswag/cartpage', views.cart_page),
+    path('dojoswag/productpage', views.product_page),
+    path('dojoswag/addcart', views.add_to_cart),
+    path('dojoswag/deletecart', views.delete_from_cart),
+    path('dojoswag/checkout', views.checkout),
+    path('dojoswag/submitorder', views.submit_order),
 ]
